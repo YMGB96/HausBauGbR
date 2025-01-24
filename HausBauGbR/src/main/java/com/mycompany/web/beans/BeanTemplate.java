074caa0b270076f67ea5568c37762b37f09d7c3c
Paandiix <ding-dong-dang@gmx.de>
2025-01-24 14:39:00 +0100

    unique benutzer

--- HausBauGbR/src/main/java/com/mycompany/web/beans/BeanTemplate.java
+++ HausBauGbR/src/main/java/com/mycompany/web/beans/BeanTemplate.java
@@ -3,152 +3,175 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.web.beans;
+
 import com.mycompany.model.template.ModelTemplate;
 import com.mycompany.services.ServiceTemplate;
 import jakarta.faces.application.FacesMessage;
 import jakarta.faces.context.FacesContext;
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.List; 
+import java.util.List;
 import org.primefaces.PrimeFaces;
 
+public abstract class BeanTemplate<T extends ModelTemplate, S extends ServiceTemplate> implements Serializable {
+    //-------------------------------------------------------------------------
+    //  Constructor(s)
+    //-------------------------------------------------------------------------     
 
-public abstract class BeanTemplate<T extends ModelTemplate, S extends ServiceTemplate> implements Serializable
-{
-  //-------------------------------------------------------------------------
-  //  Constructor(s)
-  //-------------------------------------------------------------------------     
-    public BeanTemplate() { super(); } 
+    public BeanTemplate() {
+        super();
+    }
 
-    public BeanTemplate(S service) { this(); this.service = service; } 
-    
-    
-  //-------------------------------------------------------------------------
-  //  Service
-  //-------------------------------------------------------------------------     
+    public BeanTemplate(S service) {
+        this();
+        this.service = service;
+    }
+
+    //-------------------------------------------------------------------------
+    //  Service
+    //-------------------------------------------------------------------------     
     protected S service = null;
+
+    protected S getService() {
+        return this.service;
+    }
+
+    //-------------------------------------------------------------------------
+    //  List
+    //-------------------------------------------------------------------------     
+    protected List<T> itemList = null;
+
+    public void resetItemList() {
+        itemList = null;
+        updateContentForm();
+    }
+
+    ;
     
-    protected S getService() { return this.service; }
-    
-    
-  //-------------------------------------------------------------------------
-  //  List
-  //-------------------------------------------------------------------------     
-    protected List<T> itemList = null; 
-    
-    public void resetItemList() 
-    { 
-        itemList = null; 
-        updateContentForm(); 
-    };
-    
-    public List<T> getItemList()
-    {
-        if (itemList == null) 
-        { 
-            try            
-            {
-                itemList = getService().getItemList(); 
-            } 
-            catch (Exception e) { this.itemList = null; }
-            if (itemList == null) { itemList = new ArrayList<>(); }
+    public List<T> getItemList() {
+        if (itemList == null) {
+            try {
+                itemList = getService().getItemList();
+            } catch (Exception e) {
+                this.itemList = null;
+            }
+            if (itemList == null) {
+                itemList = new ArrayList<>();
+            }
         }
         return itemList;
     }
 
-    public void setItemList(List itemList)
-    {
+    public void setItemList(List itemList) {
         this.itemList = itemList;
     }
-    
 
-  //-------------------------------------------------------------------------
-  //  Item
-  //-------------------------------------------------------------------------     
-    protected T item = null; 
-    
-    public T getItem() { if (item == null) { item = (T) getService().getNewItemInstance(); } return item; }
+    //-------------------------------------------------------------------------
+    //  Item
+    //-------------------------------------------------------------------------     
+    protected T item = null;
 
-    public void setItem(T item) { this.item = item; }
-    
-    public long getItemId() { return getItem().getId(); }
-    
-    public void setItemId(long id) 
-    { 
-        try        
-        {
-            setItem((T) getService().getDao().getById(id)); 
-        } 
-        catch (Exception e)
-        {
+    public T getItem() {
+        if (item == null) {
+            item = (T) getService().getNewItemInstance();
+        }
+        return item;
+    }
+
+    public void setItem(T item) {
+        this.item = item;
+    }
+
+    public long getItemId() {
+        return getItem().getId();
+    }
+
+    public void setItemId(long id) {
+        try {
+            setItem((T) getService().getDao().getById(id));
+        } catch (Exception e) {
             handleException("Fehler", e);
         }
     }
-    
-    public void resetItem() { this.item = null; }
-    
-    public void saveItem(T item)
-    {
-        if (item == null) { return; } 
-        if (!item.hasId()) { resetItemList(); }
-        try        
-        {
-            if (getService().getDao().save(item) > 0) { handleInfo("OK", "Eintrag gespeichert."); } 
-        } 
-        catch (Exception e)
-        {
+
+    public void resetItem() {
+        this.item = null;
+    }
+
+    public void saveItem(T item) {
+        if (item == null) {
+            return;
+        }
+        if (!item.hasId()) {
+            resetItemList();
+        }
+        try {
+            if (getService().getDao().save(item) > 0) {
+                handleInfo("OK", "Eintrag gespeichert.");
+            }
+        } catch (Exception e) {
             System.out.println(">>> ERROR --> " + e.getMessage());
             handleException("Fehler", e);
         }
-        resetItemList(); 
+        resetItemList();
     }
-    
-    public void saveItem() { saveItem(this.item); } 
- 
-    public void removeItem(T item) 
-    {  
-        try        
-        {
-            if (getService().getDao().deleteItem(item)) { handleInfo("OK", "Eintrag entfernt."); }
-        } 
-        catch (Exception e)
-        {
+
+    public void saveItem() {
+        saveItem(this.item);
+    }
+
+    public void removeItem(T item) {
+        try {
+            if (getService().getDao().deleteItem(item)) {
+                handleInfo("OK", "Eintrag entfernt.");
+            }
+        } catch (Exception e) {
             handleException("Fehler", e);
         }
-        resetItemList(); 
+        resetItemList();
     }
-    
-    public void removeItem() { removeItem(this.item); }
-    
 
-    
-  //-------------------------------------------------------------------------
-  //  Else ... 
-  //-------------------------------------------------------------------------     
-    protected void setMessage(String comonentId, FacesMessage.Severity type, String header, String msg) { FacesContext.getCurrentInstance().addMessage(comonentId, new FacesMessage(type, header, msg)); }    
-    
-    protected void setErrorMessage(String header, String msg) { setMessage(null, FacesMessage.SEVERITY_ERROR, header, msg); }
-    
-    protected void setInfoMessage(String header, String msg) { setMessage(null, FacesMessage.SEVERITY_INFO, header, msg); }
-    
-    public void componentAjaxUpdate(String s) { if (s!=null && !s.isBlank()) { PrimeFaces.current().ajax().update(s); } }
-     
-    public void updateContentForm() { componentAjaxUpdate(":contentForm"); }
+    public void removeItem() {
+        removeItem(this.item);
+    }
 
-    public void updateMessageBox() { componentAjaxUpdate(":messageBox"); }
+    //-------------------------------------------------------------------------
+    //  Else ... 
+    //-------------------------------------------------------------------------     
+    protected void setMessage(String comonentId, FacesMessage.Severity type, String header, String msg) {
+        FacesContext.getCurrentInstance().addMessage(comonentId, new FacesMessage(type, header, msg));
+    }
 
-    protected void handleException(String header, Exception e) 
-    {
+    protected void setErrorMessage(String header, String msg) {
+        setMessage(null, FacesMessage.SEVERITY_ERROR, header, msg);
+    }
+
+    protected void setInfoMessage(String header, String msg) {
+        setMessage(null, FacesMessage.SEVERITY_INFO, header, msg);
+    }
+
+    public void componentAjaxUpdate(String s) {
+        if (s != null && !s.isBlank()) {
+            PrimeFaces.current().ajax().update(s);
+        }
+    }
+
+    public void updateContentForm() {
+        componentAjaxUpdate(":contentForm");
+    }
+
+    public void updateMessageBox() {
+        componentAjaxUpdate(":messageBox");
+    }
+
+    protected void handleException(String header, Exception e) {
         setErrorMessage(header, e.getMessage());
         updateMessageBox();
     }
-    
-    protected void handleInfo(String header, String msg) 
-    {
+
+    protected void handleInfo(String header, String msg) {
         setInfoMessage(header, msg);
         updateMessageBox();
     }
-    
-    
-    
+
 }
--- HausBauGbR/src/main/java/com/mycompany/web/beans/BenutzerBean.java
+++ HausBauGbR/src/main/java/com/mycompany/web/beans/BenutzerBean.java
@@ -1,10 +1,8 @@
 package com.mycompany.web.beans;
  
-
 import com.mycompany.model.classes.Adresse;
 import com.mycompany.model.classes.Benutzer;
 import com.mycompany.services.BenutzerService;
-import java.util.Arrays;
 import java.util.List;
 import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.SessionScoped;
@@ -12,53 +10,79 @@ import jakarta.faces.context.FacesContext;
 import jakarta.inject.Named;
 import jakarta.servlet.http.HttpSession;
 
-
 @Named(value = "benutzer") 
 @SessionScoped 
-public class BenutzerBean extends BeanTemplate<Benutzer, BenutzerService>
-{
-  //-------------------------------------------------------------------------
-  //  Constructor(s)
-  //-------------------------------------------------------------------------     
-    public BenutzerBean() { super(new BenutzerService()); } 
+public class BenutzerBean extends BeanTemplate<Benutzer, BenutzerService> {
 
-    @PostConstruct
-    public void init()
-    { 
-        resetItem();
-        Adresse adresse1 = new Adresse("Teststraße", "86", "12345", "Testort", "Testland");
-        Benutzer benutzer1 = new Benutzer(true, "admin", "admin", adresse1, "123456", "0049123456789", "admin@admin.com", "admin", "admin");
-        Benutzer benutzer2 = new Benutzer(false, "user", "user", adresse1, "654321", "0049987654321", "user@user.com", "user", "user");
-        getItemList().add(benutzer1);
-        getItemList().add(benutzer2);
-        saveItem(benutzer1);
-        saveItem(benutzer2);
+    public BenutzerBean() {
+        super(new BenutzerService());
     }
 
+    @PostConstruct
+    public void init() {
+        resetItem();
 
+        Adresse adresse1 = new Adresse("Teststraße", "86",
+                "12345", "Testort", "Testland");
+        Benutzer benutzer1 = new Benutzer(true, "admin",
+                "admin", adresse1, "123456",
+                "0049123456789", "admin@admin.com",
+                "admin", "admin");
+        Benutzer benutzer2 = new Benutzer(false, "user",
+                "user", adresse1, "654321",
+                "0049987654321", "user@user.com",
+                "user", "user");
 
- 
-    
+        try {
+            // Wenn die Datenbank frisch erstellt wurde und es noch keine
+            // Benutzer gibt, werden diese zwei Benutzer standardmäßig
+            // hinzugefügt
+            List<Benutzer> benutzerInDB = getService().getDao().list();
+            boolean match = benutzerInDB.stream()
+                    .anyMatch(benutzer -> benutzer.getBenutzerName()
+                            .equals(benutzer1.getBenutzerName()));
+            if (!match) {
+                getItemList().add(benutzer1);
+                saveItem(benutzer1);
+            }
+            match = benutzerInDB.stream()
+                    .anyMatch(benutzer -> benutzer.getBenutzerName()
+                            .equals(benutzer2.getBenutzerName()));
+            if (!match) {
+                getItemList().add(benutzer2);
+                saveItem(benutzer2);
+            }
+        } catch (Exception e) {
+            System.err.println("Konnte keine Liste finden. " + e);
+        }
+    }
+
   //-------------------------------------------------------------------------
   //  Benutzerdaten
   //-------------------------------------------------------------------------     
     protected String benutzername = null;
     protected String passwort = null;
 
-    public String getBenutzername() { return benutzername; }
+    public String getBenutzername() {
+        return benutzername;
+    }
 
-    public void setBenutzername(String benutzername) { this.benutzername = benutzername; }
+    public void setBenutzername(String benutzername) {
+        this.benutzername = benutzername;
+    }
 
-    public String getPasswort() { return passwort; }
+    public String getPasswort() {
+        return passwort;
+    }
 
-    public void setPasswort(String passwort) { this.passwort = passwort; }
-      
-    
+    public void setPasswort(String passwort) {
+        this.passwort = passwort;
+    }
+
   //-------------------------------------------------------------------------
   //  Login / Logout
   //-------------------------------------------------------------------------     
-    public String doLogin() 
-    {  
+    public String doLogin() {
         /*
             CHECK USER CREDENTIALS HERE !!!
             If correct, then store the MyLoginObject in the session --> websession.setAttribute("MyLoginObject", true) 
@@ -70,15 +94,12 @@ public class BenutzerBean extends BeanTemplate<Benutzer, BenutzerService>
         websession.setAttribute("MyLoginObject", true); 
         // The return parameter is the navigation path to the next website.
         return "backend/empty.xhtml?faces-redirect=true"; 
-    } 
-    
-    public String doLogout() 
-    {  
+    }
+
+    public String doLogout() {
         HttpSession websession = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(true); 
         websession.removeAttribute("MyLoginObject"); 
         return "/login.xhtml?faces-redirect=true";
-    }  
-    
-    
-    
+    }
+
 }
